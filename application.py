7ebed2086cd12ccf2521ec0cd6a2fa15c5c1fab1
Maik Larooij <mklarooij@gmail.com>
2020-01-13 18:32:21 +0000

    login, register and spotify auth

--- application.py
+++ application.py
@@ -6,7 +6,10 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 
+import spotipy
+import startup
 
 # Configure application
 app = Flask(__name__)
@@ -14,22 +17,164 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///musiclink.db")
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route("/", methods=["GET"])
-def home():
-    return render_template("home.html")
-
-
-@app.route("/start", methods=["GET"])
 def start():
+
     return render_template("start.html")
 
-@app.route("/search", methods=["GET", "POST"])
-def search():
-    if request.method == "POST":
-        search = request.form.get("search")
-        types = request.form.get("type")
-        return render_template("searched.html", search=search, types=types)
+@app.route("/home", methods=["GET", "POST"])
+@login_required
+def home():
+    if request.method == "GET":
+        oauth = startup.getAccessToken()[0]
+        spotify = spotipy.Spotify(auth=oauth)
+        full_name = spotify.current_user()["display_name"]
+        return render_template("home.html", hoi=full_name)
 
     else:
-        return render_template("search.html")
+         return render_template("home.html")
 
+
+@app.route("/authorise", methods=["POST", "GET"])
+def authorise():
+    response = startup.getUser()
+    return redirect(response)
+
+@app.route('/callback')
+def callback():
+    startup.getUserToken(request.args['code'])
+    if session.get("user_id") != None:
+        return redirect("/home")
+    else:
+        return redirect("/register")
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+
+        # Ensure username and password were submitted, password and confirmation must match
+        if not request.form.get("username") or not request.form.get("password") or request.form.get("password") != request.form.get("confirmation"):
+            return apology("please fill in all fields")
+
+        # Ensure that the username is not taken
+        if len(db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))) != 0:
+            return apology("username already exists")
+
+        # Insert the username and hashed password into users database
+        else:
+            oauth = startup.getAccessToken()[0]
+            db.execute("INSERT INTO users (username, hash, auth) VALUES (:username, :password, :oauth)",
+                        username=request.form.get("username"), password=generate_password_hash(request.form.get("password"),
+                        method='pbkdf2:sha256', salt_length=8), oauth=oauth)
+
+
+        # Redirect user to home page
+        return redirect("/home")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["userid"]
+
+        # Redirect user to home page
+        return redirect("/authorise")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route('/search', methods=["GET", "POST"])
+@login_required
+def search():
+	if request.method == "POST":
+		oauth = startup.getAccessToken()[0]
+		spotify = spotipy.Spotify(auth=oauth)
+		artists = []
+		pictures = []
+		input = request.form.get("search")
+		albums = spotify.search(q='artist:' + input, type='artist')["artists"]["items"]
+
+		for album in albums:
+			artists.append(album['name'])
+			if len(album["images"]) != 0:
+				pictures.append(album['images'][2]['url'])
+			else:
+				pictures.append('https://image.shutterstock.com/image-vector/prohibition-no-photo-sign-vector-260nw-449151856.jpg')
+
+		recent = spotify.current_user_recently_played()['items']
+
+		recent = [recen['track']['name'] for recen in recent]
+
+		return render_template("searched.html", artists=artists, pictures=pictures, recent=recent)
+
+	else:
+		return render_template("search.html")
