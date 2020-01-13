8d6dc2c289ad87957edc3366eadb3da9d5c974d2
Maik Larooij <mklarooij@gmail.com>
2020-01-13 13:11:28 +0000

    layout and flask test

---
+++ application.py
@@ -0,0 +1,19 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
